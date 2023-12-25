97fc62dcd1595719529443ac6a5ad8dfc4c54765
Oliver Atkinson <oliveratkinsoneng@gmail.com>
2023-12-24 23:10:13 -0700

    Add place-holder return type for http methods. This took way too long to figure out how to do. :/

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use rocket::{routes, fs::FileServer, serde::json::{Json, serde_json::json, Value}, get};
+use rocket::{
+    routes,
+    response,
+    response::Responder,
+    Request,
+    Response,
+    fs::FileServer,
+    serde::json::{Json, serde_json::json, Value},
+    get, http::Status
+};
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
@@ -15,17 +24,30 @@ async fn main() -> Result<(), rocket::Error> {
 fn example() -> Json<Value> {
     let x = json!({
             "id": 20,
-            "status": "Active",
-            "type": "AIR CONDITIONING",
-            "category": "",
-            "subcategory": "",
-            "item": "Orifice Tube",
-            "description": "Orifice Tube",
-            "descriptionfull": "Orifice Tube  38623",
-            "qoh": 18,
-            "cost": 1.32,
             "price": 11.06
         });
     
     Json(x)
+}
+
+/// Allows request from all origins with the 
+/// status passed to the struct. This is needed
+/// for open apis, otherwise your client might
+/// get annoyed at the CORS errors.
+///
+/// This will work as a default return type for 
+/// http methods.
+///
+/// You can use this directly or just copy / modify
+/// it for your needs
+struct OpenApiResponse {
+    status: Status
+}
+impl<'r> Responder<'r, 'static> for OpenApiResponse {
+    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
+        Response::build()
+            .status(self.status)
+            .raw_header("Access-Control-Allow-Origin", "*")
+            .ok()
+    }
 }
